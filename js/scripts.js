604af42b1dc009860fcd540c8f172907b2060d73
Lan Dam <ldam77@yahoo.com>
2018-06-01 09:40:04 -0700

    add script to store answer from form

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#questionaire").submit(function(event){
+    event.preventDefault();
+
+    var answer1 = $("input:radio[name=question1]:checked").val();
+    var answer2 = $("input:radio[name=question2]:checked").val();
+    var answer3 = $("input:radio[name=question3]:checked").val();
+    var answer4 = $("input:radio[name=question4]:checked").val();
+    var answer5 = $("input:radio[name=question5]:checked").val();
+
+  });
+
+});
