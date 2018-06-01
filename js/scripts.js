63555942164d6712a537ffbab1834cd1e0285741
Lan Dam <ldam77@yahoo.com>
2018-06-01 11:37:24 -0700

    add branching scripts for the rest of the choices

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,23 @@ $(document).ready(function() {
       $(".suggested-track").text("Design");
       $("#class-link").attr("href", "https://www.epicodus.com/design");
     } else{
-
+      if(webOrSoftware === "web"){
+        if(startupOrEstablished === "startup"){
+          $(".suggested-track").text("Ruby");
+          $("#class-link").attr("href", "https://www.epicodus.com/ruby");
+        } else{
+          $(".suggested-track").text("PHP");
+          $("#class-link").attr("href", "https://www.epicodus.com/php");
+        }
+      } else{
+        if(businessOrAndroid === "business"){
+          $(".suggested-track").text("C#");
+          $("#class-link").attr("href", "https://www.epicodus.com/c-sharp");
+        } else{
+          $(".suggested-track").text("Java");
+          $("#class-link").attr("href", "https://www.epicodus.com/java");
+        }
+      }
     }
 
     $("#suggestion").show();
